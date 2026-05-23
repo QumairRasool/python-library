94ee7dddfd460d9d5aa832478c237b209a6ee7f4
QumairRasool <qumairrasool@gmail.com>
2026-05-23 23:33:13 +0530

    addded attempt counter

--- number guessing game.py
+++ number guessing game.py
@@ -6,8 +6,9 @@ if b == "yes" or b == "Yes" or b == "y" or b == "Y":
     while b == "yes" or b == "Yes" or b == "y" or b == "Y":
         a = random.randint(1, 100)
         x = -1                       
-
-        while x != a:                 
+        c = 0
+        while x != a: 
+                        
             x = int(input("enter a number "))
             if x == a:
                 print("you have guessed the number!")
@@ -15,7 +16,9 @@ if b == "yes" or b == "Yes" or b == "y" or b == "Y":
                 print("you have guessed a smaller number than the random number")
             elif x > a:
                 print("you have guessed a larger number than the random number")
+            c += 1
 
+        print("you guessed the number in {} attempts!".format(c))
         b = input("do you want to play again? (yes/no) ")  
 else:
     print("Okay, maybe next time!")
