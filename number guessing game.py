e99c2cb2e02013fd663883245656f3b73a0dcb8b
QumairRasool <qumairrasool@gmail.com>
2026-05-23 22:48:16 +0530

    added if condition

--- number guessing game.py
+++ number guessing game.py
@@ -1,8 +1,10 @@
 import random
 a= random.randint(1,100)
-#random number which will be generated will be stored in this variable
+b=input("welcome to the number guessing game, do you want to play? (yes/no) ")
 x=-1
-while x!=a:
+
+if b=="yes" or b=="Yes" or b=="y" or b=="Y":
+    while x!=a:
         x= int(input("enter a number "))
         if x==a:
             print ("you have guessed the number")
@@ -10,6 +12,5 @@ while x!=a:
             print ("you have guessed a smaller number than the original number")
         elif x>a:
             print ("you have guessed a larger number than the original number")
-
-
-print ("the end of program")
+else:
+    print("Okay, maybe next time!")
