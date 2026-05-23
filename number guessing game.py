c2ae9e84d4f86e78eef44c3788f5246355d5480e
QumairRasool <qumairrasool@gmail.com>
2026-05-23 23:45:11 +0530

    added won/lost flag

--- number guessing game.py
+++ number guessing game.py
@@ -5,24 +5,29 @@ b = input("welcome to the number guessing game, do you want to play? (yes/no) ")
 if b == "yes" or b == "Yes" or b == "y" or b == "Y":
     while b == "yes" or b == "Yes" or b == "y" or b == "Y":
         a = random.randint(1, 100)
-        x = -1                       
+        x = -1
         c = 0
-        while x != a: 
-            if c <7:
+        won = False                 
 
-             x = int(input("enter a number "))
-             c += 1
-             if x == a:
-                print("you have guessed the number!")
-             elif x < a:
-                print("you have guessed a smaller number than the random number")
-             elif x > a:
-                print("you have guessed a larger number than the random number")
-             if c >= 7 and x != a:       # ← check after every guess
-                print(f"you have used all your attempts! the number was {a}")
-                break    
+        while x != a:
+            if c < 7:
+                x = int(input("enter a number "))
+                c += 1
+                if x == a:
+                    print("you have guessed the number!")
+                    won = True      
+                elif x < a:
+                    print("you have guessed a smaller number than the random number")
+                elif x > a:
+                    print("you have guessed a larger number than the random number")
+                if c >= 7 and x != a:
+                    print(f"you have used all your attempts! the number was {a}")
+                    break
 
-        print("you guessed the number in {} attempts!".format(c))
-        b = input("do you want to play again? (yes/no) ")  
+        if won:                   
+            print(f"you guessed the number in {c} attempts!")
+
+        b = input("do you want to play again? (yes/no) ")
+
 else:
     print("Okay, maybe next time!")
