748211f19a14b0d18ff6ca317f3d21ab2df286ed
QumairRasool <qumairrasool@gmail.com>
2026-05-23 23:19:08 +0530

    changed some spellings

--- number guessing game.py
+++ number guessing game.py
@@ -5,18 +5,17 @@ b = input("welcome to the number guessing game, do you want to play? (yes/no) ")
 if b == "yes" or b == "Yes" or b == "y" or b == "Y":
     while b == "yes" or b == "Yes" or b == "y" or b == "Y":
         a = random.randint(1, 100)
-        x = -1                         # ← inside outer loop
+        x = -1                       
 
-        while x != a:                  # ← inside outer loop
+        while x != a:                 
             x = int(input("enter a number "))
             if x == a:
                 print("you have guessed the number!")
             elif x < a:
-                print("you have guessed a smaller number than the original number")
+                print("you have guessed a smaller number than the random number")
             elif x > a:
-                print("you have guessed a larger number than the original number")
+                print("you have guessed a larger number than the random number")
 
-        b = input("do you want to play again? (yes/no) ")  # ← outside inner loop, inside outer loop
-
+        b = input("do you want to play again? (yes/no) ")  
 else:
     print("Okay, maybe next time!")
