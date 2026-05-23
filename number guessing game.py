424d45c3f8970b6b4dbb3cdd4fd22825b0f454e3
QumairRasool <qumairrasool@gmail.com>
2026-05-23 23:13:20 +0530

    number guessing game with play again feature

--- number guessing game.py
+++ number guessing game.py
@@ -1,16 +1,22 @@
 import random
-a= random.randint(1,100)
-b=input("welcome to the number guessing game, do you want to play? (yes/no) ")
-x=-1
 
-if b=="yes" or b=="Yes" or b=="y" or b=="Y":
-    while x!=a:
-        x= int(input("enter a number "))
-        if x==a:
-            print ("you have guessed the number")
-        elif x<a:
-            print ("you have guessed a smaller number than the original number")
-        elif x>a:
-            print ("you have guessed a larger number than the original number")
+b = input("welcome to the number guessing game, do you want to play? (yes/no) ")
+
+if b == "yes" or b == "Yes" or b == "y" or b == "Y":
+    while b == "yes" or b == "Yes" or b == "y" or b == "Y":
+        a = random.randint(1, 100)
+        x = -1                         # ← inside outer loop
+
+        while x != a:                  # ← inside outer loop
+            x = int(input("enter a number "))
+            if x == a:
+                print("you have guessed the number!")
+            elif x < a:
+                print("you have guessed a smaller number than the original number")
+            elif x > a:
+                print("you have guessed a larger number than the original number")
+
+        b = input("do you want to play again? (yes/no) ")  # ← outside inner loop, inside outer loop
+
 else:
     print("Okay, maybe next time!")
